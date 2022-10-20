e59341ded05ec60042c1b6a8b3443f5747ea4722
오정환 <wjdghks6086@naver.com>
2022-10-20 15:31:31 +0900

    [Feat] BE : 프로젝트 생성

---
+++ BE/letscamp/src/test/java/com/b308/letscamp/LetscampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b308.letscamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetscampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
