724af93535aff38e8bb7f11299d245ceb4d89d5a
DESKTOP-KVCQHCD\SSAFY <dang2013@naver.com>
2022-10-27 16:42:53 +0900

    [Feat] BE : 추천 알고리즘 수정

--- core/core/recomm/views.py
+++ core/core/recomm/views.py
@@ -35,6 +35,8 @@ def recommend_campings(request, category, animal, keywords):
 
         df = campings[is_right]
 
+        print(df.head(5)['name'])
+
         # 입력받은 동반유형 키워드를 데이터 프레임에 추가
         new_data = {
             'name': ['선택 답변 키워드'],
@@ -51,14 +53,15 @@ def recommend_campings(request, category, animal, keywords):
 
         # 유사도 높은 순 정렬
         sorted_ind = keywords_sim.argsort()[:, ::-1]
-        base_index = campings_df.shape[0] - 1
-        similar_indexes = sorted_ind[base_index, 1:11]
+        base_index = new_df.shape[0] - 1
+        similar_indexes = sorted_ind[base_index]
+        result = pd.DataFrame(similar_indexes)
+        result.columns = ['인덱스']
+        new_result = result[result['인덱스'].isin(df.index.to_list())].head(10)
 
-        similar_indexes = similar_indexes.reshape(-1)
-
-        return Response(similar_indexes, status=status.HTTP_200_OK)
+        return Response(new_result.values, status=status.HTTP_200_OK)
     # 반려 동물 동반일 경우
-    else:
+    elif animal == '동반':
         # 반려동물 동반이 가능하며 선택 category와 일치하는 캠핑장만
         is_right = (campings['category'].str.contains(
             category)) & (campings['animal'] == '가능')
@@ -74,6 +77,8 @@ def recommend_campings(request, category, animal, keywords):
         new_df = pd.DataFrame(new_data)
         campings_df = pd.concat([df, new_df], ignore_index=True)
 
+        print(campings_df.head(10)['animal'])
+
         # 데이터프레임 유사도 계산
         count_vect = CountVectorizer(min_df=0, ngram_range=(1, 2))
         keywords_mat = count_vect.fit_transform(campings_df['keywords'])
@@ -81,9 +86,10 @@ def recommend_campings(request, category, animal, keywords):
 
         # 유사도 높은 순 정렬
         sorted_ind = keywords_sim.argsort()[:, ::-1]
-        base_index = campings_df.shape[0] - 1
-        similar_indexes = sorted_ind[base_index, 1:11]
+        base_index = new_df.shape[0] - 1
+        similar_indexes = sorted_ind[base_index]
+        result = pd.DataFrame(similar_indexes)
+        result.columns = ['인덱스']
+        new_result = result[result['인덱스'].isin(df.index.to_list())].head(10)
 
-        similar_indexes = similar_indexes.reshape(-1)
-
-        return Response(similar_indexes, status=status.HTTP_200_OK)
+        return Response(new_result.values, status=status.HTTP_200_OK)
