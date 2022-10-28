118904c11fe7fb53247e11e49cc4abe44ef52205
김형주 <ghj6282@daum.net>
2022-10-28 16:52:58 +0900

    [Feat] FE : api 호출 연습 및 추천 결과 페이지 연결

--- FE/frontend/src/Components/RecommResultPage/CampSite.js
+++ FE/frontend/src/Components/RecommResultPage/CampSite.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const CampSite = () => {
+  // 캠핑장 추천 장소 리스트
+  const [recommendCampList, setRecList] = useState(null);
+
+  // 캠핑장 추천장소 api 호출
+  // const RecCampLoad = async () => {
+  //   const RecCampData = await getRec();
+  //   if (RecCampData.code === 200) setRecList(RecCampData);
+  // };
+
+  // 전역 변수로 리스트 컨트롤
+
   return (
     <>
       <h4>당신에게 알맞은 캠핑장은</h4>
