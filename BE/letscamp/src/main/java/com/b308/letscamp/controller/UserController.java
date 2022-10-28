de40659c4f7c7cdb607d0e7f1eff582a32011fd5
오정환 <wjdghks6086@naver.com>
2022-10-28 17:14:22 +0900

    [feat] BE : spring security  로그인

--- BE/letscamp/src/main/java/com/b308/letscamp/controller/UserController.java
+++ BE/letscamp/src/main/java/com/b308/letscamp/controller/UserController.java
@@ -3,15 +3,23 @@ package com.b308.letscamp.controller;
 import com.b308.letscamp.dto.review.ReviewUpdateRequest;
 import com.b308.letscamp.dto.user.*;
 import com.b308.letscamp.entity.User;
+import com.b308.letscamp.jwt.JwtTokenProvider;
+import com.b308.letscamp.jwt.TokenInfo;
 import com.b308.letscamp.service.user.UserService;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static javax.security.auth.message.AuthStatus.FAILURE;
 import static javax.security.auth.message.AuthStatus.SUCCESS;
@@ -21,6 +29,33 @@ import static javax.security.auth.message.AuthStatus.SUCCESS;
 @Api(tags = {"User API"})
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+//    private final Response response;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final RedisTemplate redisTemplate;
+
+    @GetMapping("/user/login/{id}/{pw}")
+    public ResponseEntity<?> login(@PathVariable String id, @PathVariable String pw){
+        // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken =new UsernamePasswordAuthenticationToken(id,pw);
+
+        // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
+        // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenInfo tokenInfo = jwtTokenProvider.generateToken(authentication);
+
+        // TODO:: RefreshToken Redis 저장
+
+        redisTemplate.opsForValue().set(authentication.getName(), tokenInfo.getRefreshToken(),tokenInfo.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
+
+        return new ResponseEntity<> (tokenInfo,  HttpStatus.OK);
+
+
+    }
 
     @PostMapping("/user/regist")
     @ApiOperation(value = "회원가입", notes = "회원가입 요청")
@@ -29,6 +64,7 @@ public class UserController {
             @ApiResponse(code = 500, message = "서버 에러")
     })
     public UserSaveResponse create(@RequestBody @ApiParam(value = "가입할 회원 정보가 담긴 객체", required = true) UserSaveRequest request) {
+        request.setUserPw(passwordEncoder.encode(request.getUserPw()));
         Long id = userService.create(request);
         return new UserSaveResponse(id);
     }
---
+++ BE/letscamp/src/main/java/com/b308/letscamp/security/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.b308.letscamp.security;
+
+import com.b308.letscamp.entity.User;
+import com.b308.letscamp.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> findUser = userRepository.findByUserId(username);
+        if (findUser.isEmpty()) {
+            throw new UsernameNotFoundException(username);
+        }
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(findUser.get().getNickName())
+                .password(findUser.get().getUserPw())
+                .roles(findUser.get().getRole()).build();
+    }
+}
