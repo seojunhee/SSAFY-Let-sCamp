d792c148d25306cc06e9d6a3e1cf5977af223cbc
김형주 <ghj6282@daum.net>
2022-10-31 10:59:48 +0900

    [Feat] FE : ReserveBtn Component 추가

---
+++ FE/frontend/src/Components/Reserve/ReserveBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ReserveBtn = (props) => {
+
+
+  return (
+    <div>
+      <button>
+        예약하기
+      </button>
+    </div>
+  )
+};
+
+export default ReserveBtn;
--- FE/frontend/src/pages/ReservePage.js
+++ FE/frontend/src/pages/ReservePage.js
@@ -4,6 +4,7 @@ import React from "react"
 import Header from "../Components/Header/Header.js";
 import CampSiteInfo from "../Components/Reserve/CampSiteInfo.js";
 import ReservationInfo from "../Components/Reserve/ReservationInfo.js";
+import ReserveBtn from "../Components/Reserve/ReserveBtn.js";
 
 import { useSetRecoilState } from "recoil";
 import { pageNameState } from "../Store/state.js";
@@ -25,6 +26,8 @@ const Reserve = () => {
       <h3>예약 정보</h3>
       <ReservationInfo title="날짜" content="2022년 11월 25일 ~ 27일"/>
       <ReservationInfo title="인원" content="성인 2명 유아 1명 반려동물 1마리"/>
+      <hr style={{"border": "solid 1px black", "backgroundColor" : "black"}}/>
+      <ReserveBtn />  
 
     </div>
   )
