6386e7fa310c792cf4553fd76e5529632bf57280
seojunhee <watermelons3@naver.com>
2022-10-31 14:22:34 +0900

    [Design] FE : 로그인, 회원가입 페이지 css 작업

--- FE/frontend/src/pages/SignUpPage.js
+++ FE/frontend/src/pages/SignUpPage.js
@@ -1,36 +1,48 @@
 import React from "react";
-import { useSetRecoilState } from "recoil";
 import { Link } from "react-router-dom";
-import { pageNameState } from "../Store/state";
 import Header from "../Components/Header/Header.js";
+import "./style/SignUp.css";
 
 const SignUp = () => {
-  const setPageName = useSetRecoilState(pageNameState);
-
-  setPageName("회원가입");
-
   return (
     <div className="SignUp">
       <Header pageName={"회원가입"}></Header>
-      <div>회원정보를 입력해주세요</div>
-      <div>
-        <input placeholder="아이디(이메일)" />
+      <div className="SignUp-subtitle-box">
+        <div className="SignUp-subtitle">회원정보를 입력해주세요</div>
+      </div>
+      <div className="SignUp-input-box">
+        <div>
+          <input placeholder="아이디(이메일)" className="SignUp-input" />
+          <hr className="SignUp-line" />
+        </div>
+        <div>
+          <input placeholder="닉네임" className="SignUp-input" />
+          <hr className="SignUp-line" />
+        </div>
+        <div>
+          <input placeholder="주소" className="SignUp-input" />
+          <hr className="SignUp-line" />
+        </div>
+        <div>
+          <input
+            placeholder="비밀번호"
+            className="SignUp-input"
+            type="password"
+          />
+          <hr className="SignUp-line" />
+        </div>
+        <div>
+          <input
+            placeholder="비밀번호 확인"
+            className="SignUp-input"
+            type="password"
+          />
+          <hr className="SignUp-line" />
+        </div>
       </div>
       <div>
-        <input placeholder="닉네임" />
-      </div>
-      <div>
-        <input placeholder="주소" />
-      </div>
-      <div>
-        <input placeholder="비밀번호" />
-      </div>
-      <div>
-        <input placeholder="비밀번호 확인" />
-      </div>
-      <div>
-        <Link to="/signupsucess">
-          <button>회원가입</button>
+        <Link to="/mypage">
+          <button className="SignUp-submit">회원가입</button>
         </Link>
       </div>
     </div>
