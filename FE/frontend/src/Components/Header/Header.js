228cb2e0af21269655865cfdc52e5e2248d7e617
seojunhee <watermelons3@naver.com>
2022-10-31 14:24:47 +0900

    [Design] 헤더 css 임시 수정

--- FE/frontend/src/Components/Header/Header.js
+++ FE/frontend/src/Components/Header/Header.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Header.css";
 
 const Header = ({ pageName }) => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <div className="">
+      <div className="header">
         <span
-          className="back"
+          className="header-back"
           onClick={() => {
             navigate(-1);
           }}
         >
           &lt;
         </span>
-        <span>{pageName}</span>
+        <span className="header-pagename">{pageName}</span>
       </div>
+      <hr className="header-line" />
     </div>
   );
 };
