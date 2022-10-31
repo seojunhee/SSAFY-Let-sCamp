b9e6dd26fe11dc2f04a6d8de5756ed5b6f8c928f
김형주 <ghj6282@daum.net>
2022-10-31 09:58:40 +0900

    [Feat] FE : 예약페이지 초안

--- FE/frontend/src/pages/ReservePage.js
+++ FE/frontend/src/pages/ReservePage.js
@@ -1,1 +1,19 @@
-    
+import React from "react"
+
+// Components
+import Header from "../Components/Header/Header.js";
+
+
+import { Link, useNavigate } from "react-router-dom";
+
+const Reserve = () => {
+  
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+
+}
+
+export default Reserve;
