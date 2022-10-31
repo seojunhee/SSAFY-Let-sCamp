a4f066e77394f4813509227d3785a2bec73dedaa
seojunhee <watermelons3@naver.com>
2022-10-31 14:26:17 +0900

    [Design] 시작 페이지 css 요소 배치(미완)

--- FE/frontend/src/pages/StartPage.js
+++ FE/frontend/src/pages/StartPage.js
@@ -1,36 +1,31 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./style/StartPage.css";
 
 const Start = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="start">
-      <div>초보자를 위한 캠핑 추천 서비스</div>
-      <div>렛츠 캠프!</div>
-      <div>
-        <Link to="/question">
-          <button>가입 없이 바로 추천받으러 가기 </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/login">
-          <button>로그인</button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/signup">
-          <button>회원가입</button>
-        </Link>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            navigate("/mypage");
-          }}
-        >
-          마이페이지로가기
-        </button>
+    <div className="startpage">
+      <img src="/asset/campingcar.png" alt="이미지" className="startpage-img" />
+      <div className="startpage-title">초보자를 위한 캠핑 추천 서비스</div>
+      <div className="startpage-title2">렛츠 캠프!</div>
+      <div className="startpage-buttonsbox">
+        <div>
+          <Link to="/question">
+            <button className="startpage-button">
+              가입 없이 <br></br>바로 추천받으러 가기
+            </button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/login">
+            <button className="startpage-button">로그인</button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/signup">
+            <button className="startpage-button">회원가입</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
