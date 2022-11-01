7c72b62e789bbd49df07ae81646ea4864966116d
김형주 <ghj6282@daum.net>
2022-11-01 14:36:00 +0900

    [Feat] FE : 캠핑장 정보 컴포넌트 수정

--- FE/frontend/src/Components/Reserve/CampSiteInfo.js
+++ FE/frontend/src/Components/Reserve/CampSiteInfo.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// css
+import "./style/CampSiteInfo.css"
+
 const CampSite = () => {
   // 캠핑장 추천 장소 리스트
   // const [recommendCampList, setRecList] = useState(null);
@@ -14,20 +17,24 @@ const CampSite = () => {
 
   return (
     <>
-      <div className="container">
-        <div>
-          <img src="." alt="캠핑장 사진" />
+      <div className="grid">
+        <div className="item col-4">
+          <img src="../../logo192.png" alt="캠핑장 사진" />
         </div>
-        <div>
+        <div className="item col-8">
           <h3>솔내음 캠핑장</h3>
           <p>충청남도 서천군</p>
         </div>
       </div>
-      <div>
-        <h3>캠핑장 유형</h3>
-        <select name="campCategory">
-          <option value="caraban">카라반</option>
-        </select>
+      <div className="grid">
+        <h3 className="item col-4">캠핑장 유형</h3>
+        <div className="item col-8 outer-div">
+          <div className="inner-div">
+            <select name="campCategory" className="width-80 rem15">
+              <option value="caraban">카라반</option>
+            </select>
+          </div>
+        </div>
       </div>
     </>
     
