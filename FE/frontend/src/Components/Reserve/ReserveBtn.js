fe727be2de8cabc70fad9313f46e3d17e110ce5c
김형주 <ghj6282@daum.net>
2022-11-01 15:47:15 +0900

    [Feat] FE : 캠핑 정보 페이지 버튼 위치 수정

--- FE/frontend/src/Components/Reserve/ReserveBtn.js
+++ FE/frontend/src/Components/Reserve/ReserveBtn.js
@@ -4,8 +4,8 @@ const ReserveBtn = (props) => {
 
 
   return (
-    <div>
-      <button>
+    <div className="position-bottom width-100">
+      <button className="reserveButton">
         예약하기
       </button>
     </div>
