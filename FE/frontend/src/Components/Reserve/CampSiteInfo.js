a31e497e833d9216fc6a0a01a1b3e9b521eb0e31
김형주 <ghj6282@daum.net>
2022-11-01 15:31:49 +0900

    [Feat] FE : 캠핑 정보 페이지 예약 정보 수정

--- FE/frontend/src/Components/Reserve/CampSiteInfo.js
+++ FE/frontend/src/Components/Reserve/CampSiteInfo.js
@@ -21,22 +21,22 @@ const CampSite = () => {
         <div className="item col-4">
           <img src="../../logo192.png" className="img" alt="캠핑장 사진" />
         </div>
-        <div className="item col-8 outer-div">
-          <div className="outer-div">
-            <div className="">
-              <h3>솔내음 캠핑장</h3>
-            </div>
-            <div className="">
-              <p>충청남도 서천군</p>
-            </div>
+        <div className="item col-8">
+          
+          <div className="outer-div text-h3">
+            솔내음 캠핑장
           </div>
+          <div className="outer-div">
+            <p>충청남도 서천군</p>
+          </div>
+          
         </div>
       </div>
       <div className="grid">
         <h3 className="item col-4">캠핑 유형</h3>
         <div className="item col-8 outer-div">
           <div className="inner-div">
-            <select name="campCategory" className="width-80 rem15">
+            <select name="campCategory" className="width-80 rem117">
               <option value="caraban">카라반</option>
             </select>
           </div>
--- FE/frontend/src/pages/ReservePage.js
+++ FE/frontend/src/pages/ReservePage.js
@@ -23,8 +23,8 @@ const Reserve = () => {
       <div className="grid">
         <h3 className="col-4">예약 정보</h3>
       </div>
-      <ReservationInfo title="날짜" content="2022년 11월 25일 ~ 27일"/>
-      <ReservationInfo title="인원" content="성인 2명 유아 1명 반려동물 1마리"/>
+      <ReservationInfo title="날짜" content="2022년 11월 25일 ~ 2022년 11월 27일"/>
+      <ReservationInfo title="인원" content="성인 20명 유아 10명 반려동물 10마리"/>
       <hr style={{"border": "solid 1px black", "backgroundColor" : "black"}}/>
       <ReserveBtn />  
 
