193321bfd3c914ad3fc49c70c7a2107507a3f318
seojunhee <watermelons3@naver.com>
2022-11-02 16:06:44 +0900

    [Feat] FE : 상세정보 페이지 axios 연결

--- FE/frontend/src/Components/Detail/Contents.js
+++ FE/frontend/src/Components/Detail/Contents.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { campSiteState } from "../../Store/state.js";
 
 const Contents = () => {
+  const [campSiteData, SetCampSite] = useRecoilState(campSiteState);
+
   return (
     <div>
       <div>별점</div>
-      <div>위치</div>
-      <div>전화번호</div>
+      <div>위치 {campSiteData.address}</div>
+      <div>전화번호 : {campSiteData.tel}</div>
+      <div>{campSiteData.simple_des}</div>
       <hr />
-      <div>애완동물 가능 여부 : </div>
-      <div>영업 여부</div>
-      <div>영업 시즌</div>
-      <div>테마 : </div>
-      <div>썸네일</div>
+      <div>애완동물 가능 여부 : {campSiteData.animal}</div>
+      <div>영업 여부 : {campSiteData.running}</div>
+      <div>영업 시즌 : {campSiteData.running_season}</div>
+      <div>테마 : {campSiteData.theme}</div>
     </div>
   );
 };
