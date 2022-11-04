f37efa950ce209cb958ab88d18d55c7ca2e46a65
김형주 <ghj6282@daum.net>
2022-11-04 11:01:57 +0900

    [Feat] FE : 추천 결과 페이지 이미지 크기 조정

--- FE/frontend/src/Components/RecommResultPage/CampSite.js
+++ FE/frontend/src/Components/RecommResultPage/CampSite.js
@@ -3,15 +3,13 @@ import "./style/CampSite.css"
 const CampSite = (props) => {
 
   return (
-    <>
+    <div className="height-55">
       <h4>당신에게 알맞은 캠핑장은</h4>
       <h3> {(props.campSiteList[0].name)} </h3>
-      <div className="imgBox">
-        <p></p>
+      <div className="height-65 container">
         <img src={props.campSiteList[0].thumb} alt="캠핑장 사진" />
       </div>
-      
-    </>
+    </div>
   )
 };
 
