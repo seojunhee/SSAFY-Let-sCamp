b1cefe00f60721c48d7ddf7248149201b03b70eb
DESKTOP-KVCQHCD\SSAFY <hany961031@naver.com>
2022-11-07 14:18:01 +0900

    [Fix] BE : trash api result json 형태로 수정

--- BE/letscamp/src/main/java/com/b308/letscamp/controller/TrashController.java
+++ BE/letscamp/src/main/java/com/b308/letscamp/controller/TrashController.java
@@ -1,7 +1,11 @@
 package com.b308.letscamp.controller;
 
+import com.b308.letscamp.dto.trash.TrashFindResponse;
 import com.b308.letscamp.service.Trash.TrashService;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,13 @@ public class TrashController {
     private final TrashService trashService;
 
     @GetMapping("/classification/{filename}")
-    public String getClassification(@ApiParam(value = "촬영한 사진 이름", required = true) @PathVariable String filename) {
-        return trashService.classificationByImage(filename);
+    @ApiOperation(value = "해당 사진의 분류 결과", notes = "해당 사진의 분류 결과를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "요청 성공"),
+            @ApiResponse(code = 500, message = "서버 에러")
+    })
+    public TrashFindResponse getClassification(@ApiParam(value = "촬영한 사진 이름", required = true) @PathVariable String filename) {
+        String result = trashService.classificationByImage(filename).substring(1, trashService.classificationByImage(filename).length() - 1);
+        return new TrashFindResponse(result);
     }
 }
---
+++ BE/letscamp/src/main/java/com/b308/letscamp/dto/trash/TrashFindResponse.java
@@ -0,0 +1,14 @@
+package com.b308.letscamp.dto.trash;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TrashFindResponse {
+    private String result;
+}
