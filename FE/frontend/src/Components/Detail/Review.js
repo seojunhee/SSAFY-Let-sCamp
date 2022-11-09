95b0fd20c8a588f68c276fab09e20bd661a8e933
seojunhee <watermelons3@naver.com>
2022-11-09 14:05:18 +0900

    [Feat] FE : detail 페이지 피그마에 맞춰 댓글 디자인 수정 적용(1차)

--- FE/frontend/src/Components/Detail/Review.js
+++ FE/frontend/src/Components/Detail/Review.js
@@ -1,13 +1,142 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import LetsCamp from "../../api/LetsCamp";
+import "./style/Review.css";
 
-const Review = () => {
-  const [reviewdata, SetReview] = useState([]);
+const Review = (id) => {
+  const [reviewdata, SetReview] = useState();
+
+  const Rate = (reviewData) => {
+    if (reviewData.reviewData.rate > 4) {
+      return (
+        <div className="detail-rate">
+          ⭐⭐⭐⭐⭐({reviewData.reviewData.rate})
+        </div>
+      );
+    } else if (
+      reviewData.reviewData.rate > 3 &&
+      reviewData.reviewData.rate <= 4
+    ) {
+      return (
+        <div className="detail-rate">
+          ⭐⭐⭐⭐({reviewData.reviewData.rate})
+        </div>
+      );
+    } else if (
+      reviewData.reviewData.rate > 2 &&
+      reviewData.reviewData.rate <= 3
+    ) {
+      return (
+        <div className="detail-rate">⭐⭐⭐({reviewData.reviewData.rate})</div>
+      );
+    } else if (
+      reviewData.reviewData.rate > 1 &&
+      reviewData.reviewData.rate <= 2
+    ) {
+      return (
+        <div className="detail-rate">⭐⭐({reviewData.reviewData.rate})</div>
+      );
+    } else if (reviewData.reviewData.rate > 0) {
+      return (
+        <div className="detail-rate">⭐({reviewData.reviewData.rate})</div>
+      );
+    } else {
+      return <div>별점 정보가 없습니다.</div>;
+    }
+  };
+
+  const Profile = (reviewdata) => {
+    if (reviewdata.reviewdata.userExp < 500) {
+      return (
+        <div className="detail-review-imgbox">
+          <img
+            src="/asset/profile/camlin.png"
+            alt="캠린"
+            className="detail-review-profile"
+          ></img>
+        </div>
+      );
+    } else if (
+      reviewdata.reviewdata.userExp >= 500 &&
+      reviewdata.reviewdata.userExp < 1500
+    ) {
+      return (
+        <div className="detail-review-imgbox">
+          <img
+            src="/asset/profile/camding.png"
+            alt="캠딩"
+            className="detail-review-profile"
+          ></img>
+        </div>
+      );
+    } else if (
+      reviewdata.reviewdata.userExp >= 1500 &&
+      reviewdata.reviewdata.userExp < 4000
+    ) {
+      return (
+        <div>
+          <img
+            src="/asset/profile/camdeahak.png"
+            alt="캠대학생"
+            className="detail-review-profile"
+          ></img>
+        </div>
+      );
+    } else if (
+      reviewdata.reviewdata.userExp >= 4000 &&
+      reviewdata.reviewdata.userExp < 10000
+    ) {
+      return (
+        <div>
+          <img
+            src="/asset/profile/camdeahakone.png"
+            alt="캠대학원생"
+            className="detail-review-profile"
+          ></img>
+        </div>
+      );
+    } else if (reviewdata.reviewdata.userExp > 10000) {
+      return (
+        <div>
+          <img
+            src="/asset/profile/cambaksa.png"
+            alt="캠박사"
+            className="detail-review-profile"
+          ></img>
+        </div>
+      );
+    }
+  };
+
+  const List = () => {
+    return (
+      <div>
+        {reviewdata.map((reviewData, key) => (
+          <div className="detail-review" key={key}>
+            <div className="detail-reviewbox">
+              <Profile reviewdata={reviewData}></Profile>
+              <div>
+                <div>
+                  <div>{reviewData.userNickName}</div>
+                </div>
+                <Rate reviewData={reviewData}></Rate>
+                <hr className="detail-contents-line"></hr>
+                <div className="">
+                  <div className="detail-review-content">
+                    {reviewData.comment}
+                    {reviewData.date}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
   useEffect(() => {
-    const id = "1";
-    const url = LetsCamp.review.get(id);
+    const url = LetsCamp.review.get(id.id);
     axios
       .get(url, {
         headers: {
@@ -25,7 +154,7 @@ const Review = () => {
       });
   }, []);
 
-  return <div>{reviewdata ? <div></div> : <div>리뷰없음 </div>}</div>;
+  return <div>{reviewdata ? <List></List> : <div>리뷰없음 </div>}</div>;
 };
 
 export default Review;
