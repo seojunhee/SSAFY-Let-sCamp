6cae969aca3868f179bfada1e145e35aa3337391
김형주 <ghj6282@daum.net>
2022-11-10 09:46:37 +0900

    [Feat] FE : 분리수거 페이지 이미지 미리보기

--- FE/frontend/src/pages/RecyclePage.js
+++ FE/frontend/src/pages/RecyclePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 // Component
@@ -7,10 +7,42 @@ import Loading from "../Components/Recycle/Loading.js";
 
 const Recycle = () => {
 
+  const [loading, setLoading] = useState(false);
+  const [files, setFiles] = useState("");
+  
+
+  const onLoadFile = (e) => {
+    setFiles(e.target.files)
+
+  };
+
+  useEffect(() => {
+    preview();
+
+    return () => preview();
+  })
+
+  const preview = () => {
+    if (!(files)) return false;
+    
+    const imgEl = document.querySelector('.img__box')
+
+    const reader = new FileReader();
+
+    reader.onload = () => (imgEl.style.backgroundImage = `url(${reader.result})`);
+
+    reader.readAsDataURL(files[0]);
+  };
+
+  
+
   return (
     <div className="App">
-      <Loading />
+      {loading ? <Loading /> : null}
       <Header />
+      <div className="width-100 height-55 img__box">
+      </div>
+      <input type="file" onChange={onLoadFile}/>
     </div>
   )
 }
