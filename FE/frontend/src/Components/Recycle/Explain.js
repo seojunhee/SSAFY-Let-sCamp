bebe30194d89e07d0421a7f057ff2d9632883b6d
김형주 <ghj6282@daum.net>
2022-11-10 10:06:50 +0900

    [Feat] FE : 분리수거 페이지 배치

---
+++ FE/frontend/src/Components/Recycle/Explain.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Explain = () => {
+  
+};
+
+export default Explain;
--- FE/frontend/src/pages/RecyclePage.js
+++ FE/frontend/src/pages/RecyclePage.js
@@ -4,17 +4,24 @@ import { Link } from "react-router-dom";
 // Component
 import Header from '../Components/Header/Header.js'
 import Loading from "../Components/Recycle/Loading.js";
+import Navbar from "../Components/NavBar/NavBar.js"
 
 const Recycle = () => {
 
   const [loading, setLoading] = useState(false);
   const [files, setFiles] = useState("");
+  const [isComplete, SetIsComplete] = useState(false)
   
 
   const onLoadFile = (e) => {
     setFiles(e.target.files)
 
   };
+
+  const onScanning = () => {
+    SetIsComplete(!isComplete)
+
+  }
 
   useEffect(() => {
     preview();
@@ -43,6 +50,16 @@ const Recycle = () => {
       <div className="width-100 height-55 img__box">
       </div>
       <input type="file" onChange={onLoadFile}/>
+      <div className="height-15">
+        {isComplete? "이건 무슨 물건입니다. 분리수거를 잘 해주세요": "쓰레기 사진을 올리면 분석하여 알려드립니다."}
+      </div>
+      <div className="container">
+        <button onClick={onScanning}>분석하기</button>
+        <Link to={"/main"}>
+          <button>메인으로</button>
+        </Link>
+      </div>
+      <Navbar />
     </div>
   )
 }
