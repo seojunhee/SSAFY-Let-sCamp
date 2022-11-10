47759877ca8e54adea0f2826d2d1ffcf5c4d593b
seojunhee <watermelons3@naver.com>
2022-11-10 16:37:11 +0900

    [Feat] FE : 메인페이지 예약 정보 없을 경우 기능 및 디자인 추가

--- FE/frontend/src/Components/MainPage/Main.js
+++ FE/frontend/src/Components/MainPage/Main.js
@@ -5,16 +5,27 @@ import { useNavigate } from "react-router-dom";
 const Main = () => {
   const navigate = useNavigate();
   return (
-    <div className="">
-      <div>같이 캠핑을 떠나볼까요?</div>
-      <button>예약하기</button>
-      <button
-        onClick={() => {
-          navigate("/question");
-        }}
-      >
-        캠핑장 추천받기
-      </button>
+    <div className="mainpage-main">
+      <hr></hr>
+      <div className="mainpage-main-box">
+        <img src="/asset/search.gif" alt="" className="mainpage-main-img"></img>
+        <div className="mainpage-main-text">
+          당신에게 딱 맞는 <br></br> 캠핑장을 추천받으려면?
+          <div className="mainpage-main-navigation">추천받기&gt;</div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="mainpage-main-box">
+        <div className="mainpage-main-text">
+          가고싶은 캠핑장<br></br> 검색하고 예약까지!
+          <div className="mainpage-main-navigation">검색하러 가기&gt;</div>
+        </div>
+        <img
+          src="/asset/calender.gif"
+          alt=""
+          className="mainpage-main-img"
+        ></img>
+      </div>
     </div>
   );
 };
