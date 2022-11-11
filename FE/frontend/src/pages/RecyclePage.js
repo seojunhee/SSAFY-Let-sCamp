8e2d90748b12acfa076c586e77cb660dec52c861
김형주 <ghj6282@daum.net>
2022-11-11 10:56:46 +0900

    분리수거 페이지 인풋버튼 수정

--- FE/frontend/src/pages/RecyclePage.js
+++ FE/frontend/src/pages/RecyclePage.js
@@ -137,7 +137,10 @@ const Recycle = () => {
       <div className="width-100 height-55">
         <img src={attachment} className="img-cover width-100 height-100"></img>
       </div>
-      <input type="file" onChange={onLoadFile}/>
+      <div className="filebox">
+        <label for="file">파일찾기</label> 
+        <input type="file" id="file" onChange={onLoadFile}/>
+      </div>
       <div className="height-15 outer-div">
         {isComplete? `${trash}입니다. 분리수거를 잘 해주세요`: "쓰레기 사진을 올리면 분석하여 알려드립니다."}
       </div>
