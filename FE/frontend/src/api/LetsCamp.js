90f98c7fdf70427b7bc37d182f811c41ffc19e85
seojunhee <watermelons3@naver.com>
2022-11-14 16:05:58 +0900

    [Feat] FE : API 추가

--- FE/frontend/src/api/LetsCamp.js
+++ FE/frontend/src/api/LetsCamp.js
@@ -10,11 +10,12 @@ const Hate = "hate";
 const Glamping = "glamping";
 const Caraban = "caraban";
 const CarCamping = "carCamping";
+const Task = "task";
 
 const letsCamp = {
   camping: {
     getAll: () => Host + Camping,
-    getOne: (id) => Host + Camping + "/" + `${id}`,
+    getOne: (id) => Host + Camping + `/${id}`,
     getRecomm: (category, animal, keywords) =>
       Host +
       Camping +
@@ -24,65 +25,70 @@ const letsCamp = {
       `${animal}/` +
       `${keywords}/`,
     getRandom: () => Host + Camping + "/random",
-    serchByName: (name) => Host + Camping + "/searchbyname/" + `${name}`,
-    searchByDoSi: (dosi) => Host + Camping + "/searchbydosi/" + `${dosi}`,
+    serchByName: (name) => Host + Camping + `/searchbyname/${name}`,
+    searchByDoSi: (dosi) => Host + Camping + `/searchbydosi/${dosi}`,
   },
   user: {
-    login: (id, pw) => Host + User + "/login/" + `${id}/` + `${pw}`,
+    login: (id, pw) => Host + User + `/login/${id}/${pw}`,
     regist: () => Host + User + "/regist",
     update: () => Host + User + "/update",
-    updateExp: (exp) => Host + User + "/update/" + `${exp}`,
-    duCheck: (userId) => Host + User + "/check/" + `${userId}`,
+    updateExp: (exp) => Host + User + `/update/${exp}`,
+    duCheck: (userId) => Host + User + `/check/${userId}`,
     info: () => Host + User + "/myInfo",
   },
   review: {
     change: () => Host + Review,
-    write: (campingId) => Host + Review + "/"`${campingId}`,
+    write: (campingId) => Host + Review + `/${campingId}`,
     get: (campingId) => Host + Review + `/${campingId}`,
-    delete: (id) => Host + Review + "/"`${id}`,
+    delete: (id) => Host + Review + `/${id}`,
     getUserReview: () => Host + Review + "/user",
   },
   reservation: {
     getReserve: () => Host + Reservation,
-    reserve: (campingId) => Host + Reservation + "/" + `${campingId}`,
-    delete: (reservationId) => Host + Reservation + "/" + `${reservationId}`,
+    reserve: (campingId) => Host + Reservation + `/${campingId}`,
+    delete: (reservationId) => Host + Reservation + `/${reservationId}`,
   },
   hate: {
     getHate: () => Host + Hate,
-    setHate: (id) => Host + Hate + "/" + `${id}`,
-    deleteHate: (id) => Host + Hate + "/" + `${id}`,
+    setHate: (id) => Host + Hate + `/${id}`,
+    deleteHate: (id) => Host + Hate + `/${id}`,
   },
   normal: {
     change: () => Host + NormalCamping,
-    getAll: (reservationId) => Host + NormalCamping + "/" + `${reservationId}`,
-    add: (reservationId) => Host + NormalCamping + "/" + `${reservationId}`,
-    delete: (reservationId) => Host + NormalCamping + "/" + `${reservationId}`,
+    getAll: (reservationId) => Host + NormalCamping + `/${reservationId}`,
+    add: (reservationId) => Host + NormalCamping + `/${reservationId}`,
+    delete: (reservationId) => Host + NormalCamping + `/${reservationId}`,
     getByLevel: (reservationId, level) =>
-      Host + NormalCamping + "/" + `${reservationId}/` + `${level}`,
+      Host + NormalCamping + `/${reservationId}/${level}`,
   },
   glamping: {
     change: () => Host + Glamping,
-    getAll: (reservationId) => Host + Glamping + "/" + `${reservationId}`,
-    add: (reservationId) => Host + Glamping + "/" + `${reservationId}`,
-    delete: (reservationId) => Host + Glamping + "/" + `${reservationId}`,
+    getAll: (reservationId) => Host + Glamping + `/${reservationId}`,
+    add: (reservationId) => Host + Glamping + `/${reservationId}`,
+    delete: (reservationId) => Host + Glamping + `/${reservationId}`,
     getByLevel: (reservationId, level) =>
-      Host + Glamping + "/" + `${reservationId}/` + `${level}`,
+      Host + Glamping + `/${reservationId}/${level}`,
   },
   caraban: {
     change: () => Host + Caraban,
-    getAll: (reservationId) => Host + Caraban + "/" + `${reservationId}`,
-    add: (reservationId) => Host + Caraban + "/" + `${reservationId}`,
-    delete: (reservationId) => Host + Caraban + "/" + `${reservationId}`,
+    getAll: (reservationId) => Host + Caraban + `/${reservationId}`,
+    add: (reservationId) => Host + Caraban + `/${reservationId}`,
+    delete: (reservationId) => Host + Caraban + `/${reservationId}`,
     getByLevel: (reservationId, level) =>
-      Host + Caraban + "/" + `${reservationId}/` + `${level}`,
+      Host + Caraban + `/${reservationId}/${level}`,
   },
   carcamping: {
     change: () => Host + CarCamping,
-    getAll: (reservationId) => Host + CarCamping + "/" + `${reservationId}`,
-    add: (reservationId) => Host + CarCamping + "/" + `${reservationId}`,
-    delete: (reservationId) => Host + CarCamping + "/" + `${reservationId}`,
+    getAll: (reservationId) => Host + CarCamping + `/${reservationId}`,
+    add: (reservationId) => Host + CarCamping + `/${reservationId}`,
+    delete: (reservationId) => Host + CarCamping + `/${reservationId}`,
     getByLevel: (reservationId, level) =>
-      Host + CarCamping + "/" + `${reservationId}/` + `${level}`,
+      Host + CarCamping + `/${reservationId}/${level}`,
+  },
+  task: {
+    get: (reservationId) => Host + Task + `/${reservationId}`,
+    check: (reservationId, level, subLevel) =>
+      Host + Task + `/${reservationId}/${level}/${subLevel}`,
   },
 };
 
