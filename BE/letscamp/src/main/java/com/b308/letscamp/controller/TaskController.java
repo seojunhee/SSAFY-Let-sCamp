2f50765c1f0113c7143676ec76107ad1a725dbac
오정환 <wjdghks6086@naver.com>
2022-11-14 11:19:56 +0900

    [feat] BE : task swagger 추가

--- BE/letscamp/src/main/java/com/b308/letscamp/controller/TaskController.java
+++ BE/letscamp/src/main/java/com/b308/letscamp/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.b308.letscamp.controller;
 import com.b308.letscamp.dto.task.TaskDTO;
 import com.b308.letscamp.repository.TaskRepository;
 import com.b308.letscamp.service.task.TaskService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +22,19 @@ import java.util.List;
 public class TaskController {
 
     private final TaskService taskService;
-
+    @ApiOperation(value = "해당 예약의 전체 일정 조회", notes = "해당 예약의 일정 리스트를 조회한다.")
     @GetMapping("/{reservationId}")
-    public ResponseEntity<List<TaskDTO>> getList(@PathVariable Long reservationId) {
+    public ResponseEntity<List<TaskDTO>> getList(@ApiParam(value = "예약 Id", required = true) @PathVariable Long reservationId) {
         return new ResponseEntity<>(taskService.getTasks(reservationId), HttpStatus.OK);
     }
 
-    @GetMapping("/{reservationId}/{level}/{sublevel}")
-    public ResponseEntity<Boolean> changeStatus(@PathVariable Long reservationId, @PathVariable int level, @PathVariable int sublevel) {
+    @ApiOperation(value = "일정 상태 변경", notes = "해당 일정의 상태 변경")
+   @GetMapping("/{reservationId}/{level}/{sublevel}")
+    public ResponseEntity<Boolean> changeStatus(
+            @ApiParam(value = "예약 ID", required = true) @PathVariable Long reservationId,
+            @ApiParam(value = "현재 레벨", required = true) @PathVariable int level,
+            @ApiParam(value = "sub 레벨", required = true) @PathVariable int sublevel
+    ) {
         return new ResponseEntity<>(taskService.changeTaskStatus(reservationId, level, sublevel), HttpStatus.OK);
     }
 
